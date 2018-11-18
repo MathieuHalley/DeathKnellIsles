ab86b483de2119a4e2698fd4c2b7ab58b1d05909
Mathieu Halley <mathieu.halley@gmail.com>
2018-11-18 11:21:21 +1000

    .

---
+++ Scripts/Obsolete/IInfluenceFlowCoordinator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    public interface IInfluenceFlowCoordinator
+    {
+        void PropagateInfluence(IEnumerable<IInfluenceNode> nodes, IEnumerable<Influences> influences);
+        IList<IInfluenceNode> RankNodeNeighboursByInfluence(IEnumerable<IInfluenceNode> nodes, Influences influence);
+    }
+}
